56fa8a28687dae2f7b0b94a6b02d55ed830a0730
Sara Jo Jeiter-Johnson <sjeiterj@u.rochester.edu>
2022-06-23 13:34:57 -0400

    changed testing h5

--- testing_h5files.py
+++ testing_h5files.py
@@ -1,8 +1,9 @@
-import numpy as np  
-import matplotlib.pyplot as plt  
+import numpy as np
+import matplotlib.pyplot as plt
 import h5py
+import imageio
 
-train_h5 = h5py.File('dataset/mnist/train.h5', 'r')
+train_h5 = h5py.File('dataset/cifar-10/cifar-10-train.h5', 'r')
 print(list(train_h5.keys()))
 
 images = train_h5['images']
@@ -10,12 +11,9 @@ labels = train_h5['labels']
 print(images.shape)
 print(labels.shape)
 
-print(labels[0:12]) #shows labels
+print(labels[0:6]) #shows labels
 
-plt.subplots(3,4,figsize = (20,20))
-
-for i in range(12):
-    img_np = images[i]
-    plt.subplot(3,4,1+i)
-    plt.imshow(img_np)
-plt.show() #shows correspoding images
+for i in range(6):
+    imdata = np.array(images[i][:,:,:])
+    imfile = 'test' + str(i) + '.jpg'
+    imageio.imwrite(imfile, imdata)
